4501fca5d3463adfcac5a8289683d5cb953df2cf
vinhnv <vinhnv.dev@gmail.com>
2021-01-07 01:18:35 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  const [isOne,setIsOne] = useState(String)
+  return (
+    <Text>Hello, I am your cat!</Text>
+  );
+}
+
+export default App;

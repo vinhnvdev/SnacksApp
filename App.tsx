292608d8499c09e8852007c43b29509168f21f02
vinhnv <vinhnv.dev@gmail.com>
2021-01-13 22:54:01 +0700

    no message

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
-import React,{useState} from 'react';
-import { Text } from 'react-native';
+import React, {useState} from 'react';
+import {Text} from 'react-native';
 
 const App = () => {
-  const [isOne,setIsOne] = useState(String)
-  return (
-    <Text>Hello, I am your cat "cac ban!</Text>
-  );
-}
+  const [isOne, setIsOne] = useState(String);
+  return <Text>Hello, I am your cat "cac ban!</Text>;
+};
 export default App;
